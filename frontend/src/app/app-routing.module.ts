7efda1ece444b37600411fe45d4e9ff46f876dd1
Anlele1701 <lethanhduyan@gmail.com>
2023-10-12 20:54:37 +0700

    Hoàn thiện layout admin
    Sử dụng Collaspe thay cho Sidebar. Tạo adminbox, sửa lỗi liên quan đến responsive, sidebar. Chưa hoàn thiện position sticky

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './client/about-us/about-us.component';
 import { PurchaseHistoryComponent } from './client/purchase-history/purchase-history.component';
 import { MembershipClassComponent } from './client/membership-class/membership-class.component';
+import { ProductDetailComponent } from './client/product-detail/product-detail.component';
+import { SidebarComponent } from './admin/sidebar/sidebar.component';
 
 const routes: Routes = [
   { path: 'about-us', component: AboutUsComponent },
   { path: 'purchase-history', component: PurchaseHistoryComponent },
   { path: 'membership-class', component: MembershipClassComponent },
+  { path: 'product-detail', component: ProductDetailComponent},
+  { path: 'admin', component: SidebarComponent}
 ];
 
 @NgModule({
