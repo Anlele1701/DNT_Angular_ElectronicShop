5e2a535031342920f2c3101f210d00dff6204e1c
daibangdangcap <nguyentienthanh1122003@gmail.com>
2023-12-02 01:23:55 +0700

    hoàn thành hiển thị dữ liệu giỏ hàng lên giao diện giỏ hàng, thanh toán
    thêm chức năng đăng xuất

--- frontend/src/app/client/purchase/purchase.component.ts
+++ frontend/src/app/client/purchase/purchase.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from 'src/app/services/cartService/cart.service';
+import { cartItem } from 'src/app/services/cartService/cartItem.service';
+import { UserServiceService } from 'src/app/services/userService/user-service.service';
 
 @Component({
   selector: 'app-purchase',
   templateUrl: './purchase.component.html',
-  styleUrls: ['./purchase.component.css']
+  styleUrls: ['./purchase.component.css'],
+  providers: [cartItem]
 })
-export class PurchaseComponent {
+export class PurchaseComponent implements OnInit{
+  constructor(private userService:UserServiceService, private cartService: CartService, private router: Router){}
+  userInfo:any
+  cartList:cartItem[]
+  tamTinh: number=0
+  tongSL: number=0
+  getUserInfo()
+  {
+    this.userInfo=this.userService.getUser()
+  }
 
+  getCartList()
+  {
+    this.cartList=this.cartService.getCart()
+  }
+
+  getTamTinh(){
+    this.tamTinh=this.cartService.updateTongTien()
+  }
+
+  getTongSL(){
+    this.tongSL=this.cartService.updateTongSLMua()
+  }
+
+  ngOnInit(): void {
+      this.getUserInfo()
+      this.getCartList()
+      this.getTamTinh()
+      this.getTongSL()
+  }
 }
--- frontend/src/app/client/shopping-cart/shopping-cart.component.ts
+++ frontend/src/app/client/shopping-cart/shopping-cart.component.ts
@@ -1,18 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { CartService } from 'src/app/services/cartService/cart.service';
+import { cartItem } from 'src/app/services/cartService/cartItem.service';
+import { UserServiceService } from 'src/app/services/userService/user-service.service';
+import { LoginRegisComponent } from '../login-regis/login-regis.component';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.css']
+  styleUrls: ['./shopping-cart.component.css'],
+  providers:[cartItem]
 })
-export class ShoppingCartComponent {
-  count=0;
-  counter(type:string){
+export class ShoppingCartComponent implements OnInit{
+  constructor(private cartService: CartService, private cartitem:cartItem, private userService: UserServiceService, private router:Router, private popup: MatDialog){}
+  tongTien: number
+  tongSLMua: number
+  cartList:cartItem[]=[]
+  counter(type:string, idSP: string, giaTien: number, slHienCo: number){
     if(type === 'add'){
-      this.count++
+      if(this.showSoLuongMua(idSP)+1<=slHienCo)
+      {
+        this.tongTien=this.tongTien+giaTien
+        this.tongSLMua++
+      }
+      return this.cartService.plusOneItem(idSP)
     }
-    else if(type==='minus' && this.count >0){
-      this.count--
+    else{
+      if(this.showSoLuongMua(idSP)-1>=1)
+      {
+        this.tongTien=this.tongTien-giaTien
+        this.tongSLMua--
+      }
+      return this.cartService.minusOneItem(idSP)
+    }
+  }
+
+  showSoLuongMua(idSP){
+    return this.cartService.showSLMua(idSP)
+  }
+
+  showThanhTien(idSP){
+    return this.cartService.updateThanhTien(idSP)
+  }
+
+  getCartList(){
+    this.cartList=this.cartService.getCart()
+  }
+
+  ngOnInit(): void {
+    this.getCartList()
+    this.tongTien=this.cartService.updateTongTien()
+    this.tongSLMua=this.cartService.updateTongSLMua()
+  }
+
+  deleteItemCart(idSP: string){
+    this.cartList=this.cartService.deleteItem(idSP)
+    this.tongSLMua=this.cartService.updateTongSLMua()
+    this.tongTien=this.cartService.updateTongTien()
+  }
+
+  deleteAll()
+  {
+    this.cartList=this.cartService.deleteAll()
+    this.tongSLMua=0
+    this.tongTien=0
+  }
+
+  changeToPurchasePage(){
+    if(this.userService.checkLogin()){
+      this.router.navigate(['/client/purchase'])
+    }else{
+      const popup = this.popup.open(LoginRegisComponent,{
+      });
+      popup.afterClosed().subscribe(result =>{
+        console.log(result);
+      })
     }
   }
 }
