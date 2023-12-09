8f205f71138d616754e0169e35bf44ceb8210b99
daibangdangcap <nguyentienthanh1122003@gmail.com>
2023-12-09 11:12:12 +0700

    fix lỗi sau khi merge nhánh main mất hàm thanh toán vnpay của minh

--- backend/src/controllers/donHangController.js
+++ backend/src/controllers/donHangController.js
@@ -103,27 +103,19 @@ var getvnPayIPN = async (req,res) => {
         let hmac = crypto.createHmac('sha512', secretKey);
         let signed = hmac.update(new Buffer(signData, 'utf-8')).digest('hex');
 
-        let paymentStatus = '0'; // Giả sử '0' là trạng thái khởi tạo giao dịch, chưa có IPN. Trạng thái này được lưu khi yêu cầu thanh toán chuyển hướng sang Cổng thanh toán VNPAY tại đầu khởi tạo đơn hàng.
-        //let paymentStatus = '1'; // Giả sử '1' là trạng thái thành công bạn cập nhật sau IPN được gọi và trả kết quả về nó
-        //let paymentStatus = '2'; // Giả sử '2' là trạng thái thất bại bạn cập nhật sau IPN được gọi và trả kết quả về nó
-
-        let checkOrderId = true; // Mã đơn hàng "giá trị của vnp_TxnRef" VNPAY phản hồi tồn tại trong CSDL của bạn
-        let checkAmount = true; // Kiểm tra số tiền "giá trị của vnp_Amout/100" trùng khớp với số tiền của đơn hàng trong CSDL của bạn
+        let paymentStatus = '0';
+        let checkOrderId = true;
+        let checkAmount = true;
         if (secureHash === signed) {
-            //kiểm tra checksum
             if (checkOrderId) {
                 if (checkAmount) {
                     if (paymentStatus == '0') {
-                        //kiểm tra tình trạng giao dịch trước khi cập nhật tình trạng thanh toán
+                
                         if (rspCode == '00') {
-                            //thanh cong
-                            //paymentStatus = '1'
-                            // Ở đây cập nhật trạng thái giao dịch thanh toán thành công vào CSDL của bạn
+                            
                             res.status(200).json({ RspCode: '00', Message: 'Success' });
                         } else {
-                            //that bai
-                            //paymentStatus = '2'
-                            // Ở đây cập nhật trạng thái giao dịch thanh toán thất bại vào CSDL của bạn
+                           
                             res.status(200).json({ RspCode: '00', Message: 'Success' });
                         }
                     } else {
@@ -167,7 +159,6 @@ var vnPayReturn = async (req,res) => {
         let signed = hmac.update(new Buffer(signData, 'utf-8')).digest('hex');
 
         if (secureHash === signed) {
-            //Kiem tra xem du lieu trong db co hop le hay khong va thong bao ket qua
             res.status(200).json({ code: vnp_Params['vnp_ResponseCode'] });
         } else {
             res.status(500).json({code: '97'})
